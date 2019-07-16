c0d079c65f967c6d7d535803f4379405105f129a
TomDonnly <49160210+TomDonnly@users.noreply.github.com>
2019-07-16 12:57:53 -0400

    new repo

---
+++ modules/chronograf-security-group-rules/variables.tf
@@ -0,0 +1,37 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "security_group_id" {
+  description = "The ID of the Security Group to which all the rules should be attached."
+  type        = string
+}
+
+variable "http_port" {
+  description = "The HTTP port that Chronograf will listen on for connections"
+  type        = number
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "http_port_cidr_blocks" {
+  description = "The list of IP address ranges in CIDR notation from which to allow connections to the http_port."
+  type        = list(string)
+  default     = []
+}
+
+variable "http_port_security_groups" {
+  description = "The list of Security Group IDs from which to allow connections to the http_port. If you update this variable, make sure to update var.http_port_security_groups_num too!"
+  type        = list(string)
+  default     = []
+}
+
+variable "http_port_security_groups_num" {
+  description = "The number of security group IDs in var.http_port_security_groups. We should be able to compute this automatically, but due to a Terraform limitation, if there are any dynamic resources in var.http_port_security_groups, then we won't be able to: https://github.com/hashicorp/terraform/pull/11482"
+  type        = number
+  default     = null
+}
